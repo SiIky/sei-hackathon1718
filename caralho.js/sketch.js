b104968b4abd6552978dbf41641a0f532039fcd5
Rui Fonseca <rui.fonseca@talkdesk.com>
2018-02-10 23:02:20 +0000

    Add buttons to change input

--- caralho.js/sketch.js
+++ caralho.js/sketch.js
@@ -1,99 +1,144 @@
 var mic, fft, sound, lastspectrum, M;
-var flag = 0, recState = 1;
-var numframe = 0, silencio = 0, sflag = 0;
+var flag = 0,
+  recState = -1;
+var numframe = 0,
+  silencio = 0,
+  sflag = 0;
 
-var maximaAmplitude = 17, intervaloFrames = 30;
+var maximaAmplitude = 17,
+  intervaloFrames = 30;
 
 var avaliacao = 0.0; // 0-100
 var numAvaliacoes = 0;
 
-function preload() {
-    //sound = loadSound("assets/Talk_rui.wav");
+var state = "file";
+var inputValue = "";
+var input = document.getElementById("fileInput");
+
+document.getElementById("showFile").addEventListener("click", handleFileButton);
+document.getElementById("showMic").addEventListener("click", handleMicButton);
+
+function handleFileButton() {
+  if (state === "mic" || input.value !== inputValue) {
+    inputValue = input.value;
+    //render sound
+    state = "file";
+    sound = loadSound(input.value, onLoadSuccess, onLoadFailure, whileLoading);
+  }
+}
+
+function onLoadSuccess() {
+  console.log("success");
+  sound.loop();
+  beginRender(sound);
+}
+
+function onLoadFailure() {
+  console.error("Failure fetching sound");
+}
+
+function whileLoading(percentage) {
+  console.log("loading... ", percentage);
+}
+
+function handleMicButton() {
+  if (state === "file") {
+    state = "mic";
+    mic = new p5.AudioIn();
+    mic.start();
+    beginRender(mic);
+  }
+}
+
+function beginRender(soundInput) {
+  fft = new p5.FFT();
+  fft.setInput(soundInput);
+  recState = 1;
 }
 
 function setup() {
-    createCanvas(1000,400);
-    noFill();
-
-    mic = new p5.AudioIn();
-    mic.start();
-    fft = new p5.FFT();
-    fft.setInput(mic);
-    //fft.setInput(sound);
-    //sound.loop();
+  createCanvas(1000, 400);
+  noFill();
 }
 
-function keyPressed(){
-    if(keyCode === 32){
-        if((recState === 0 || recState === 2) && mic.enabled){ // stop - recording
-            mic.start();
-            numAvaliacoes = 0;
-            avaliacao = 0.0;
-            recState = 1;
-        }else{ //recording - stop
-            mic.stop();
+function keyPressed() {
+  if (keyCode === 32) {
+    if ((recState === 0 || recState === 2) && mic.enabled) {
+      // stop - recording
+      mic.start();
+      numAvaliacoes = 0;
+      avaliacao = 0.0;
+      recState = 1;
+    } else {
+      //recording - stop
+      if (state === "mic") {
+        mic.stop();
+      } else {
+        sound.stop();
+      }
 
-            recState = 0;
-        }
+      recState = 0;
     }
+  }
 }
 
 function avaliar() {
+  var spectrum = fft.analyze();
 
-    var spectrum = fft.analyze();
-
-    if(flag === 0){
-        lastspectrum = spectrum;
-        flag++;
-    }
-
-    M = 0;
-    for (i = 0; i<spectrum.length; i++) {
-        var diff = abs(spectrum[i]-lastspectrum[i]);
-        if(M < diff){
-            M = diff;
-        }
-    }
-    if(M<maximaAmplitude){
-        silencio++;
-    }
-
-    if(numframe==intervaloFrames){
-        if(numframe-silencio <= silencio){
-            sflag = 1;
-        }else{
-            sflag = 0;
-        }
-        numframe = 0;
-        silencio = 0;
-    }
-
-    if(sflag==0){
-        background(0,255,0);
-    }else{
-        background(255,0,0);
-    }
-
-    beginShape();
-    for (i = 0; i<spectrum.length; i++) {
-        vertex(i, map(abs(spectrum[i]-lastspectrum[i]), 0, 255, 400, 0) );
-    }
-    endShape();
+  if (flag === 0) {
     lastspectrum = spectrum;
-    numframe++;
-}
+    flag++;
+  }
 
-function mostrarAvaliacao(){
-    background(200);
-    console.log("acabou");
-    text("acabou",300,500);
-    recState = 2;
-}
-
-function draw(){
-    if(recState === 1){
-        avaliar();
-    }else if(recState === 0){
-        mostrarAvaliacao();
+  M = 0;
+  for (i = 0; i < spectrum.length; i++) {
+    var diff = abs(spectrum[i] - lastspectrum[i]);
+    if (M < diff) {
+      M = diff;
     }
+  }
+  if (M < maximaAmplitude) {
+    silencio++;
+  }
+
+  if (numframe == intervaloFrames) {
+    if (numframe - silencio <= silencio) {
+      sflag = 1;
+    } else {
+      sflag = 0;
+    }
+    numframe = 0;
+    silencio = 0;
+  }
+
+  if (sflag == 0) {
+    background(0, 255, 0);
+  } else {
+    background(255, 0, 0);
+  }
+
+  beginShape();
+  for (i = 0; i < spectrum.length; i++) {
+    vertex(i, map(abs(spectrum[i] - lastspectrum[i]), 0, 255, 400, 0));
+  }
+  endShape();
+  lastspectrum = spectrum;
+  numframe++;
+}
+
+function mostrarAvaliacao() {
+  background(200);
+  console.log("acabou");
+  text("acabou", 300, 500);
+  recState = 2;
+}
+
+function draw() {
+  if (recState === 1) {
+    avaliar();
+  } else if (recState === 0) {
+    mostrarAvaliacao();
+  } else {
+    //Nada selecionado
+  }
 }
